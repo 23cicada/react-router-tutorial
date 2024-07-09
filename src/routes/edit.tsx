cfee41d8391533613b6b43da67d2eeeade831396
cicada <cicada>
2024-07-09 23:31:24 +0800

    configure lint-staged

--- src/routes/edit.tsx
+++ src/routes/edit.tsx
@@ -11,7 +11,10 @@ export async function action({ request, params }: ActionFunctionArgs) {
   const formData = await request.formData();
   // Object.fromEntries 将键值对列表转换为一个对象
   const updates = Object.fromEntries(formData);
+  console.log("test");
+
   await updateContact(params.contactId!, updates);
+
   return redirect(`/tutorial/contacts/${params.contactId}`);
 }
 
